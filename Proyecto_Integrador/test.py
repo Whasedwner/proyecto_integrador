9cae477158ea0cc8bd1b5a401c1933cad02edf9a
Whasedloper <94811395+Whasedwner@users.noreply.github.com>
2022-10-21 18:48:48 -0500

    Delete test.py

--- Proyecto_Integrador/test.py
+++
@@ -1,9 +0,0 @@
-
-
-
-res=input(f'\nCual es tu respuesta? ')
-while res.isdigit():  
-     print("Formato inccorrecto")
-     res=input(f'\nCual es tu respuesta? ')
-else:
-     print("Formato correcto") 
